3e301e0114441e50c7d66a92741ee9767fad6cb0
John Nooney <1803534@uad.ac.uk>
2024-06-16 15:47:45 +0100

    Use LRUCache

--- LRU/src/Program.cs
+++ LRU/src/Program.cs
@@ -10,7 +10,26 @@ class Program
         * int param_1 = obj.Get(key);
         * obj.Put(key,value);
         */
+        int lruCacheCapacity = 2;
+        LRUCache lruCache = new LRUCache(lruCacheCapacity);
+
+        Node node1 = new Node(1,2);
+        Node node2 = new Node(2,3);
+        Node node3 = new Node(3,1);
         
-        LRUCache lruCache = new LRUCache(2);
+        Node[] nodeArray = new Node[3] {node1, node2, node3} ;
+
+        foreach (Node node in nodeArray)
+        {
+            lruCache.Put(node.Key, node.Value);
+        }
+
+        int cacheFirstValue = lruCache.Get(1);
+        int cacheSecondValue = lruCache.Get(2);
+        int cacheThirdValue = lruCache.Get(3);
+        
+        Console.WriteLine($"Retrieved value: {cacheFirstValue} for key: 1");
+        Console.WriteLine($"Retrieved value: {cacheSecondValue} for key: 2");
+        Console.WriteLine($"Retrieved value: {cacheThirdValue} for key: 3");
     }
 }
