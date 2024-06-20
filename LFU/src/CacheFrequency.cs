5ace128ff34607dee34c22b86391cddac7296e26
John Nooney <1803534@uad.ac.uk>
2024-06-20 08:36:40 +0100

    Fix formatting

--- LFU/src/CacheFrequency.cs
+++ LFU/src/CacheFrequency.cs
@@ -14,11 +14,11 @@ public class CacheFrequency
         LinkedList<CacheNode> nodeListToUpdate;
         if(dataFrequency.TryGetValue(newNode.Value.Frequency, out nodeListToUpdate))
         {
-        nodeListToUpdate.Remove(oldNode);
-        nodeListToUpdate.AddFirst(newNode);
-        dataFrequency[newNode.Value.Key] = nodeListToUpdate;
-        
-        Console.WriteLine($"Update Node {oldNode.Value.Key},{oldNode.Value.Value} with new value: {newNode.Value.Value} at frequency: {newNode.Value.Frequency}");
+            nodeListToUpdate.Remove(oldNode);
+            nodeListToUpdate.AddFirst(newNode);
+            dataFrequency[newNode.Value.Key] = nodeListToUpdate;
+            
+            Console.WriteLine($"Update Node {oldNode.Value.Key},{oldNode.Value.Value} with new value: {newNode.Value.Value} at frequency: {newNode.Value.Frequency}");
         }
     }
 
@@ -36,8 +36,8 @@ public class CacheFrequency
         LinkedList<CacheNode> nodeListAtFrequency;
         if(!dataFrequency.TryGetValue(node.Value.Frequency, out nodeListAtFrequency))
         {
-        Console.WriteLine($"Unable to remove Node {node.Value.Key},{node.Value.Value}. No Frequency List at frequency: {node.Value.Frequency}");
-        return;
+            Console.WriteLine($"Unable to remove Node {node.Value.Key},{node.Value.Value}. No Frequency List at frequency: {node.Value.Frequency}");
+            return;
         }
         
         nodeListAtFrequency.Remove(node);
@@ -46,8 +46,8 @@ public class CacheFrequency
         // check if frequencyDictionary is empty
         if(nodeListAtFrequency.First == null)
         {
-        dataFrequency.Remove(node.Value.Frequency);
-        Console.WriteLine($"List at frequency: {node.Value.Frequency} is empty. Removed List.");
+            dataFrequency.Remove(node.Value.Frequency);
+            Console.WriteLine($"List at frequency: {node.Value.Frequency} is empty. Removed List.");
         }
     }
 
@@ -57,26 +57,26 @@ public class CacheFrequency
         //get existing Frequency list if exists, otherwise create new
         if(dataFrequency.TryGetValue(node.Value.Frequency, out nodeListAtFrequency))
         {
-        nodeListAtFrequency.AddFirst(node);
+            nodeListAtFrequency.AddFirst(node);
 
-        Console.WriteLine($"Frequency list exists at frequency: {node.Value.Frequency}. Appended Node {node.Value.Key},{node.Value.Value}.");
+            Console.WriteLine($"Frequency list exists at frequency: {node.Value.Frequency}. Appended Node {node.Value.Key},{node.Value.Value}.");
         }
         else
         {
-        nodeListAtFrequency = new LinkedList<CacheNode>();
-        nodeListAtFrequency.AddFirst(node);
+            nodeListAtFrequency = new LinkedList<CacheNode>();
+            nodeListAtFrequency.AddFirst(node);
 
-        Console.WriteLine($"Frequency list doesn't exist at frequency: {node.Value.Frequency}. Added Node {node.Value.Key},{node.Value.Value} to new list.");
+            Console.WriteLine($"Frequency list doesn't exist at frequency: {node.Value.Frequency}. Added Node {node.Value.Key},{node.Value.Value} to new list.");
         }
 
         if(dataFrequency.TryAdd(node.Value.Frequency, nodeListAtFrequency))
         {
-        Console.WriteLine($"Added Node {node.Value.Key},{node.Value.Value} to frequency: {node.Value.Frequency}.");
+            Console.WriteLine($"Added Node {node.Value.Key},{node.Value.Value} to frequency: {node.Value.Frequency}.");
         }
         else
         {
-        dataFrequency[node.Value.Frequency] = nodeListAtFrequency;
-        Console.WriteLine($"Updated frequency: {node.Value.Frequency} to add Node {node.Value.Key},{node.Value.Value}.");
+            dataFrequency[node.Value.Frequency] = nodeListAtFrequency;
+            Console.WriteLine($"Updated frequency: {node.Value.Frequency} to add Node {node.Value.Key},{node.Value.Value}.");
         }
     }
 
